ed706022672b096d581902a1dfe5cbe3f60d87e2
Hira63s <sunshine9.r8@gmail.com>
2022-12-15 16:47:28 -0500

    implemented bubble sort

--- Algorithms/bubblesort.py
+++ Algorithms/bubblesort.py
@@ -6,12 +6,14 @@ def bubblesort(arr):
 
     # start at the first element and go to the next element
     # when do we stop at bubblesort?
-    for i in range(0, len(arr)-1):
-        for k in range(0, len(arr)-1):
-            arr[i], arr[i+1]
+    for i in range(0, len(arr)):
+        already_sorted = True
+        for k in range(n-i-1):
             if arr[i] < arr[i+1]:
                 arr[i], arr[i+1] = arr[i+1], arr[i]
-
-
+            already_sorted = False
+    if already_sorted:
+        break
+    return arr
 
 print('I am bubble sort')
