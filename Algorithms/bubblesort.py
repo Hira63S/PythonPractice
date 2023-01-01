2416f182e006e6642fac3f7e84e708ebc3aa5386
Hira63s <sunshine9.r8@gmail.com>
2022-12-31 23:42:59 -0500

    cs50 algos

--- Algorithms/bubblesort.py
+++ Algorithms/bubblesort.py
@@ -17,3 +17,14 @@ def bubblesort(arr):
     return arr
 
 print('I am bubble sort')
+
+'''Counter version of bubblesort'''
+
+def bubble_sort(arr):
+    counter = -1
+    for i in range(0, len(arr)):
+        already_sorted = True
+        for k in range(len(arr)-i-1):
+            if arr[i] < arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+            counter +=1
