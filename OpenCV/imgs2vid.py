1c1ecc66bf976919f4e701c383871a6074639162
Hira K <mlengineer42@gmail.com>
2023-04-08 23:39:34 -0400

    imgs2vid

--- OpenCV/imgs2vid.py
+++ OpenCV/imgs2vid.py
@@ -4,8 +4,8 @@ import glob
 import os
 
 
-fil_dir = 'C://users/cathx/repos/nerf-pytorch/vid_images'
-img_dir = 'C://users/cathx/repos/nerf-pytorch/vid_images/*.jpg'
+fil_dir = 'C://users/cathx/repos/nerf-pytorch/images'
+img_dir = 'C://users/cathx/repos/nerf-pytorch/images/*.jpg'
 
 img_array = []
 
@@ -21,7 +21,7 @@ for filename in glob.glob(img_dir):
     img_array.append(img)
 # print(img_array)
 
-out = cv2.VideoWriter('NERF1.avi', cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+out = cv2.VideoWriter('nerf_t.avi', cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
 
 for i in range(len(img_array)):
     out.write(img_array[i])
